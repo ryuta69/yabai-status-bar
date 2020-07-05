df2f9b7e61d7fab6806082efaa79231f306d0308
ryuta69 <eyma22s.yu@gmail.com>
2020-07-06 04:40:36 +0900

    Add background

--- desktop.jsx
+++ desktop.jsx
@@ -1,4 +1,6 @@
 import get from 'lodash/fp/get';
+import Wifi from './components/wifi.jsx';
+import Battery from './components/battery.jsx';
 import { parse } from './helpers.jsx';
 
 export const command = 'sh yabai-status-bar/scripts/desktop.sh';
@@ -8,7 +10,10 @@ export const refreshFrequency = 5000;
 export const className = `
   bottom: 0;
   color: #a8a8a8;
-  font-family: Helvetica Neue;
+  width: 100%;
+  background-color: #0A0E14;
+  opacity: 0.8;
+  font-family: HackNerdFontComplete-Italic;
 
   .desktop {
     align-items: center;
@@ -19,6 +24,21 @@ export const className = `
     height: 23px;
     margin-left: 20px;
   }
+
+  .right-bar {
+    align-items: center;
+    color: #a8a8a8;
+    display: flex;
+    font-family: Helvetica Neue;
+    font-size: 14px;
+    height: 23px;
+    position: relative;
+    right: 0;
+  }
+
+  .right-bar > div {
+    margin-right: 20px;
+  }
 `;
 
 export const render = ({ output }) => {
@@ -26,7 +46,9 @@ export const render = ({ output }) => {
 
   return (
     <div className="desktop">
-      <p>{get('date_time')(data)}</p>
+      <div>{get('date_time')(data)}</div>
+      <Battery data={get('battery')(data)} />
+      <Wifi data={get('wifi')(data)} />
     </div>
   );
 };
